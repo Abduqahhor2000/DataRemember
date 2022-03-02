3eaca4742647ba7fd1258eefcbc64ab311d45140
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2022-03-02 14:24:05 +0500

    opush

--- client/src/components/screens/User.js
+++ client/src/components/screens/User.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import "./private.scss";
+import "./user.scss";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { FaUserTie, FaListUl } from 'react-icons/fa'
 
 const Private = () => {
     const State_User = useSelector(state => state.user.user)
@@ -23,9 +24,9 @@ const Private = () => {
           };
     
           try {
-            const {data} = await axios.post("/api/auth/getclient_type", {sellerID: State_User.user._id}, config).then(err=> console.log(err))
-            console.log(data)
-            setClient_type(data);
+            const data = await axios.post("/api/auth/getclient_type", {sellerID: State_User.user._id}, config)
+            console.log(data.data.data)
+            setClient_type(data.data.data);
           } catch (error) {
             console.log(error)
             setError("You are not authorized please login");
@@ -37,17 +38,27 @@ const Private = () => {
 
     return(
         <>
-            <div>
-                <div>user</div>
-                <div>
+            <div className="user">
+                <div className="panel">
+                  <div className="name">
+                   <span className="icon"><FaUserTie/></span>
+                   <span className="text">{State_User?.user?.username}</span>
+                  </div>
+                </div>
+                <div className="types">
                   {client_type.map(item => {
                     return (
-                      <div>  </div>
+                      <div className="type">
+                        <div className="type_left">
+                          <div className="type_icon"><FaListUl/></div>
+                          <div className="type_name">{item.clientType}</div>
+                          <div className="quality">{item.quality}</div>
+                        </div>
+                      </div>
                     )
                   })}
                 </div>
             </div>
-      
         </>
     )
 }
