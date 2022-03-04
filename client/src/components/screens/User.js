2f7370ddb63c5130d29215d8c6da5a8299e09ac1
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2022-03-04 17:38:51 +0500

    make it better

--- client/src/components/screens/User.js
+++ client/src/components/screens/User.js
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./user.scss";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { clearUserData } from "../../store/actions/userDataAction"
 import { FaUserTie, FaListUl } from 'react-icons/fa'
 import AddClientTypeModal from "../modal/AddClientTypeModal";
 import EditClientTypeModal from "../modal/EditClientTypeModal";
@@ -11,6 +12,7 @@ import EditUserModal from "../modal/EditUserModal";
 
 const Private = () => {
     const State_User = useSelector(state => state.user.user)
+    const dispatch = useDispatch()
     const [effect, setEffect] = useState(0)
     const navigate = useNavigate();
     const [client_type, setClient_type] = useState([])
@@ -20,6 +22,7 @@ const Private = () => {
     const [isDeleteTypeModalOpen, setIsDeleteTypeModalOpen] = useState(false)
     const [isEditUserModalOpen, setIsEditUserModalOpen] = useState(false)
     const [typeName, setTypeName] = useState("")
+    const [getAllType, setGetAllType] = useState(false)
     
     useEffect(() => {
       const fetchPrivateDate = async () => {
@@ -34,8 +37,10 @@ const Private = () => {
           try {
             const data = await axios.post("/api/auth/getclient_type", {sellerID: State_User.user._id}, config)
             setClient_type(data.data.data);
+            setGetAllType(true)
           } catch (error) {
             console.log(error)
+            setGetAllType(false)
           }
         };
         fetchPrivateDate()
@@ -59,11 +64,28 @@ const Private = () => {
                       <div className="edit_user">
                         <button onClick={()=>{setIsEditUserModalOpen(true)}} >Edit User</button>
                       </div>
+                      <div className="log_out">
+                        <button onClick={()=>{dispatch(clearUserData())}}>Edit User</button>
+                      </div>
                   </div>
                 </div>
                 <div className="types">
-                  
-                  {client_type.map(item => {
+                  {!getAllType ?  <div class="loadingio-spinner-spinner-0udingbvrrcc">
+                  <div class="ldio-3opmt1mq4co">
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                  </div>
+                </div> : client_type.map(item => {
                     return (
                       <div className="type" key={item._id}>
                         <div className="type_left">
