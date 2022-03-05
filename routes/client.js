69e2179552a5c9abfa173b7cd516fc9699c26217
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2022-03-05 11:45:22 +0500

    am

--- routes/client.js
+++ routes/client.js
@@ -4,8 +4,8 @@ const { addclient, deleteclient, getclients, updateclient } = require("../contro
 const { addconvert, deleteconvert, getconverts, updateconvert } = require("../controllers/convert")
 const { stat_client } = require("../controllers/statistic")
 
-router.route("/").post(addclient).get(getclients)
-router.route("/get").get(getclients)
+router.route("/").post(addclient)
+router.route("/get").post(getclients)
 router.route("/:clientID").delete(deleteclient).put(updateclient)
 router.route("/convert").post(addconvert).get(getconverts)
 router.route("/convert/:convertID").put(updateconvert).delete(deleteconvert)
